22e123b966f8fbb96d4160d15621268280c80ac6
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-03-01 15:31:26 +0100

    Add headers

--- init.el
+++ init.el
@@ -1,5 +1,12 @@
+;;; puppet-pack.el --- Puppet setup
+
+;;; Commentary:
+
+;;; Code:
+
 (install-packs '(puppet-mode))
 
 ;; puppet-mode for the .pp file
 (add-to-list 'auto-mode-alist '("\.pp$" . puppet-mode))
 
+;;; puppet-pack.el ends here
