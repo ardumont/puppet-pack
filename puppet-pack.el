b58200b887f181d98447e9a1c70f5649d71a33f4
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-07-30 21:36:39 +0200

    Use use-package

--- puppet-pack.el
+++ puppet-pack.el
@@ -4,15 +4,13 @@
 
 ;;; Code:
 
-(require 'install-packages-pack)
-(install-packages-pack/install-packs '(puppet-mode
-                                       smartscan))
+(use-package smartscan)
 
-(require 'puppet-mode)
-;; puppet-mode for the .pp file
-(add-to-list 'auto-mode-alist '("\.pp$" . puppet-mode))
-(require 'smartscan)
-(add-hook 'puppet-mode-hook (lambda () (smartscan-mode)))
+(use-package puppet-mode
+  :config
+  ;; puppet-mode for the .pp file
+  (add-to-list 'auto-mode-alist '("\.pp$" . puppet-mode))
+  (add-hook 'puppet-mode-hook 'smartscan-mode))
 
 (provide 'puppet-pack)
 ;;; puppet-pack.el ends here
