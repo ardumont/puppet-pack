f1bbf2688e9527c8749b57ef24ffbee9237893d1
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2018-11-06 02:09:21 +0100

    puppet-pack: Simplify pack

--- puppet-pack.el
+++ puppet-pack.el
@@ -4,13 +4,12 @@
 
 ;;; Code:
 
-(use-package smartscan)
+(require 'smartscan)
 
-(use-package puppet-mode
-  :config
-  ;; puppet-mode for the .pp file
-  (add-to-list 'auto-mode-alist '("\.pp$" . puppet-mode))
-  (add-hook 'puppet-mode-hook 'smartscan-mode))
+(require 'puppet-mode)
+;; puppet-mode for the .pp file
+(add-to-list 'auto-mode-alist '("\.pp$" . puppet-mode))
+(add-hook 'puppet-mode-hook 'smartscan-mode)
 
 (provide 'puppet-pack)
 ;;; puppet-pack.el ends here
