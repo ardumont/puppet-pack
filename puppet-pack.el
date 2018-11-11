42f5208a26921566fe8eda51ba88498b3ead3d4f
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2018-11-11 11:03:41 +0100

    Simplify puppet-mode hook
    puppet-mode is a prog-mode

--- puppet-pack.el
+++ puppet-pack.el
@@ -4,12 +4,9 @@
 
 ;;; Code:
 
-(require 'smartscan)
-
 (require 'puppet-mode)
 ;; puppet-mode for the .pp file
 (add-to-list 'auto-mode-alist '("\.pp$" . puppet-mode))
-(add-hook 'puppet-mode-hook 'smartscan-mode)
 
 (provide 'puppet-pack)
 ;;; puppet-pack.el ends here
